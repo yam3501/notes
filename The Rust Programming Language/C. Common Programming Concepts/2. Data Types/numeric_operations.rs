5b9ae6ae34c3f639b860d4036e822b21711aedc1
yam3501 <yash.mhaskar@gmail.com>
2024-01-31 19:39:26 +0530

    Updated data types

---
+++ The Rust Programming Language/C. Common Programming Concepts/2. Data Types/numeric_operations.rs
@@ -0,0 +1,25 @@
+// Numeris Operations
+
+fn main() {
+    //addition
+    let sum = 5 + 10;
+    println!("sum: {sum}");
+
+    //subraction
+    let diff = 95.5 - 4.3;
+    println!("difference: {diff}");
+
+    //multiplication
+    let prod = 4 * 30;
+    println!("product: {prod}");
+
+    //division
+    let quot = 56.7 / 32.2;
+    let truncated = -5 / 3; // results in -1
+    println!("quotient: {quot}");
+    println!("truncated part: {truncated}");
+
+    //remainder
+    let remd = 43 % 5;
+    println!("remainder: {remd}");
+}
